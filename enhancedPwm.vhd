e354558a5da80946cc10ed8f72980621df641fc8
scast3 <santiagoc333@gmail.com>
2025-09-08 16:29:25 -0600

    signals

--- enhancedPwm.vhd
+++ enhancedPwm.vhd
@@ -1,2 +1,17 @@
 library work;
 use work.basicBuildingBlocksVhdl_package.all;
+
+entity enhancedPwm is
+    Port ( clk : in STD_LOGIC;
+        resetn : in STD_LOGIC;
+        dutyCycle : in STD_LOGIC_VECTOR (8 downto 0);
+        enb : in STD_LOGIC;
+        pwmSignal : out STD_LOGIC;
+        pwmCount : out STD_LOGIC_VECTOR (7 downto 0);
+        rollOver : out STD_LOGIC);
+end enhancedPwm;
+
+architecture Behavioral of enhancedPwm is
+    signal E255, dutyGreateCnt : STD_LOGIC;
+    signal dutyCycle_int STD_LOGIC_VECTOR (8 downto 0);
+    signal pwmCount_int STD_LOGIC_VECTOR (8 downto 0): 
